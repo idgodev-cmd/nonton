1384745d07e375ba73ed744e79483fb3dda92fa4
idgodev <idgo.dev@gmail.com>
2026-03-15 10:40:46 +0700

    fix: clean merge conflict markers from all files

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 281230dd36066321a5ff845e80e39d312194aea4
 const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
@@ -17,37 +13,3 @@ module.exports = [
     },
   },
 ];
-<<<<<<< HEAD
-=======
-=======
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
->>>>>>> 877c61ce545f95ce9f64a3f0ab50d369b6897075
->>>>>>> 281230dd36066321a5ff845e80e39d312194aea4
